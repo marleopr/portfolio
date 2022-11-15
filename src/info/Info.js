4da17f3d95610f4572825a23a13dc207717ee1ca
Márleo Piber da Rosa <marleopr@hotmail.com>
2022-11-14 23:55:36 -0300

    Alterações sobre mim

--- src/info/Info.js
+++ src/info/Info.js
@@ -27,7 +27,7 @@ export const info = {
     firstName: "Márleo",
     lastName: "Piber",
     // initials: "MPR",
-    initials: <img src={icone}/>, // the example uses first and last, but feel free to use three or more if you like.
+    initials: <img src={icone} />, // the example uses first and last, but feel free to use three or more if you like.
     position: " Full Stack Developer",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
