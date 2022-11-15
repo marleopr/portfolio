b62b1ff022f2e8bb0cd1618146ca9d3779b9eef9
Márleo Piber da Rosa <marleopr@hotmail.com>
2022-11-14 23:20:11 -0300

    troca foto perfil

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.module.scss';
 import BaseLayout from "./components/BaseLayout";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 function showHiringMessage() { try { window.console.log("\n%cOl\u00e1!\n%cSou um desenvolvedor Full Stack em busca de uma oportunidade!\n\n%cEntre em contato comigo através das minhas redes sociais listadas na página inicial deste portfólio!\n%chttps://marleo-portfolio.surge.sh/\n\n", "font-family: serif; font-size: 32px; color: #767676", "font-family: serif; font-size: 25px; color: rgb(144, 39, 176); font-weight: bold;", "font-family: sans-serif; font-size: 16px; line-height: 1.2rem; color: #767676", "font-family: sans-serif; font-size: 11px; text-decoration: underline; line-height: 1.2rem; color: #767676") } catch (a) { } }
 showHiringMessage();
@@ -10,7 +10,7 @@ function App() {
    return (
       <div>
          <BrowserRouter>
-            <BaseLayout/>
+            <BaseLayout />
          </BrowserRouter>
       </div>
    );
